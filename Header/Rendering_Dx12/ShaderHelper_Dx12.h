66ecab8e5c71d8d21e59301f1b2c1232d4b9edcc
butibuti <butikouki@gmail.com>
2022-07-18 19:41:34 +0900

    調整

--- Header/Rendering_Dx12/ShaderHelper_Dx12.h
+++ Header/Rendering_Dx12/ShaderHelper_Dx12.h
@@ -18,10 +18,6 @@ public:
 	BUTIRENDERING_API static void Compile(const std::string& arg_sourceFilePath, const std::string& arg_outputFilePath, const std::string& source, const char* entryPoint, const char* shaderModel, const CompileType& type);
 	static void FXCShaderReflection(const void* arg_data, const std::int32_t arg_size, List<ConstantBufferReflection>& arg_output_list_cBuffer, List<TextureReflection>& arg_output_list_texture, List<SamplerReflection>& arg_output_list_sampler, ShaderStructReflection& arg_output_input, ShaderStructReflection& arg_output_output);
 	static void DXCShaderReflection(const void* arg_data, const std::int32_t arg_size, List<ConstantBufferReflection>& arg_output_list_cBuffer, List<TextureReflection>& arg_output_list_texture, List<SamplerReflection>& arg_output_list_sampler, ShaderStructReflection& arg_output_input, ShaderStructReflection& arg_output_output);
-	static void OutputCereal(const std::vector<ShaderElement>& v, const std::string& arg_path);
-	static void InputCereal(std::vector<ShaderElement>& v, const std::string& arg_path);
-	static void OutputCereal(const std::vector<std::int32_t>& v, const std::string& arg_path);
-	static void InputCereal(std::vector<std::int32_t>& v, const std::string& arg_path);
 };
 }
 }
