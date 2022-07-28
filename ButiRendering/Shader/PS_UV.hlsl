12f58b534b470d6c2e98cd77c911a49172a4662e
butibuti <butikouki@gmail.com>
2022-07-28 12:45:34 +0900

    修正

--- ButiRendering/Shader/PS_UV.hlsl
+++ ButiRendering/Shader/PS_UV.hlsl
@@ -3,5 +3,6 @@ Use_ObjectInformation(b1)
 float4 PSMain(Pixel_UV pixel) : SV_TARGET
 {
 	float4 Tex = (mainTexture.Sample(mainSampler,pixel.uv))*color;
+	clip(Tex.a < 0.1f ? -1 : 1);
 	return Tex;
 }
