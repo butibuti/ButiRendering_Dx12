732335da1768af4b419196594f4cb81625b4ccc1
butibuti <butikouki@gmail.com>
2022-12-27 18:52:09 +0900

    更新

---
+++ HLSLCompileSupporter/HLSLCompileSupporter.cpp
@@ -0,0 +1,24 @@
+﻿#include<fstream>
+#include <iostream>
+#include<string>
+int Compile(const std::string& arg_inputPath,const std::string& arg_outputPath) {
+
+}
+
+int main(const std::int32_t argCount, const char* args[])
+{
+	switch (argCount)
+	{
+	case 1:
+
+		break;
+	case 2:
+		Compile(args[0],args[1]);
+		break;
+	default:
+		std::cout << "入力パス 出力パス　を入力してください" << std::endl;
+		return 0;
+		break;
+	}
+
+}
